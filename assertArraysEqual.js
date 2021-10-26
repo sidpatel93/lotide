9aab8e92337fc35467ae68f77abc3400534cb81b
spatel93 <sid.auburn@gmail.com>
2021-10-26 00:57:32 +0000

    finished assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = (arr1, arr2) =>{
+  if (arr1.length  !== arr2.length) return false;
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if(eqArrays(arr1, arr2)) console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
+  else console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3])
+assertArraysEqual([1, 2, 3], [3, 2, 1])
