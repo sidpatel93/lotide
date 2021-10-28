55d0ac25609069eb3a1af5e691f14adc81acd190
spatel93 <sid.auburn@gmail.com>
2021-10-28 22:53:47 +0000

    implemented check for nested array using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -5,12 +5,16 @@ const assertEqual = (actual, expected) => {
 
 const eqArrays = (arr1, arr2) =>{
   if (arr1.length  !== arr2.length) return false;
-  else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) return false;
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if(Array.isArray(arr1[i]) || Array.isArray(arr2[i])){
+      if(!eqArrays(arr1[i], arr2[i])) return false
     }
-    return true;
+    else{
+      if(arr1[i] !== arr2[i]) return false
+    }
   }
+  return true;
 };
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
@@ -21,3 +25,8 @@ assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
