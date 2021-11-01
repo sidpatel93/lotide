96f2847d05a8588dfcd9320c8f935ece661a9dcb
spatel93 <sid.auburn@gmail.com>
2021-11-01 12:48:38 +0000

    refactor headTest to convert mocha tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,25 @@
 const assertEqual = require('../assertEqual')
 const head = require('../head')
+const assert = require('chai').assert;
 
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe('#head', ()=> {
+
+  it("return 1 for [1,2,3]", ()=>{
+    assert.strictEqual(head([1,2,3]),1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("return undefined for []", ()=>{
+    assert.strictEqual(head([]), undefined);
+  })
+
+  it("return Hello for [Hello, Lighthouse, Labs]", ()=>{
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+  })
+
+});
+
