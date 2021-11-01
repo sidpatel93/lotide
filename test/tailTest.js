9c39e0465c0a73d0448cd0983e75277382c38d5e
spatel93 <sid.auburn@gmail.com>
2021-11-01 13:03:33 +0000

    refactor tailTest for mocha test library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,18 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+const assert = require('chai').assert;
 
+describe("#tail tests",()=>{
 
-//Test 1
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+  it("should return [Lighthouse, Labs]", ()=>{
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  })
 
-//Test 2
-const result1 = tail(["Hello"]);
-assertEqual(result1.length, 0);
-assertEqual(result1[0], undefined);
+  it("should return empty array",()=>{
+    assert.deepEqual(tail(["Hello"]), [])
+  })
 
-//Test 3
-const result2 = tail([]);
-assertEqual(result2.length, 0);
-assertEqual(result2[0], undefined);
+  it("should return undefined",()=>{
+    assert.deepEqual(tail(["Hello"]), [])
+  })
+})
